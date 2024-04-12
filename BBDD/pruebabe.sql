c5d26c057d2caf8ca0bc99b31371d91cd29cd0af
Jesus Soto <soleje2862004@gmail.com>
2024-04-12 17:14:11 -0600

    Prueba Jesus Soto Ledezma

--- BBDD/pruebabe.sql
+++ BBDD/pruebabe.sql
@@ -33,6 +33,7 @@ CREATE TABLE `usuarios` (
   `apellidoPaterno` varchar(30) NOT NULL,
   `apellidoMaterno` varchar(30) NOT NULL,
   `fechaNacimiento` date NOT NULL,
+  `telefono` varchar(15) NOT NULL,
   `sexo` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- procesarRegistro.php
+++ procesarRegistro.php
@@ -1,3 +1,40 @@
 <?php 
-	echo "<h1>" . "BIENVENID@ TU CODIGO PHP VA AQUÍ" . "</h1>";
+   //Validacion de datos de la bd
+   $user = "root";
+   $pass = "";
+   $host = "localhost";
+   $dbNombre = "prueba";
+
+   //Coneccion a la base de datos
+   $connection = new mysqli($host, $user, $pass, $dbNombre);
+
+   //Deteccion de error al conectar
+   if($connection->connect_error){
+	echo "No se a podido conectar con el servidor". $connection->connect_error;
+   }
+
+   //Recuperacion de datos
+   $nombre = $_POST["nombre"];
+   $apellidoPaterno = $_POST["apellido_paterno"];
+   $apellidoMaterno = $_POST["apellido_materno"];
+   $fechaNacimiento = $_POST["fecha_nacimiento"];
+   $telefono = $_POST["telefono"];
+   $sexo = $_POST["sexo"];
+   
+   //Sentencia SQL
+   $stmt = $connection->prepare("INSERT INTO usuarios (nombre, apellidoPaterno, apellidoMaterno,fechaNacimiento,telefono,sexo)
+                       VALUES (?, ?, ?, ?, ?, ?)");
+   if ($stmt === false) {
+    die('prepare() failed: ' . htmlspecialchars($connection->error));
+   }
+   $stmt->bind_param('ssssss', $nombre, $apellidoPaterno, $apellidoMaterno, $fechaNacimiento, $telefono, $sexo);
+   $stmt->execute();
+
+
+	echo "<h1>Usuario Registrado</h1>";
+
+   $connection->close();
+
+   echo'<a href="index.html">Regresar</a>';
+
 ?>
