31710d36e97a5726abb5800033d976e7bcd23a6c
AngelRico2001 <166875517+AngelRico2001@users.noreply.github.com>
2024-04-12 17:11:49 -0600

    Update procesarRegistro.php

--- procesarRegistro.php
+++ procesarRegistro.php
@@ -1,35 +1,37 @@
 <?php
-//Configurar la conexión a la base de datos
+
 $servername = "localhost"; 
 $username = "root"; 
 $password = ""; 
 $dbname = "pruebaBE"; 
 
-// Crear conexión
+// Se establece la conexión a la base de datos
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Verifica si la conexión fue exitosa
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-//Procesar los datos del formulario
+// Se obtienen los datos enviados mediante el método POST desde un formulario HTML
 $nombre = $_POST['nombre'];
 $apellidoPaterno = $_POST['apellido_paterno'];
 $apellidoMaterno = $_POST['apellido_materno'];
-$localidad = $_POST['localidad'];
 $fechaNacimiento = $_POST['fecha_nacimiento'];
 $sexo = $_POST['sexo'];
+$correo = $_POST['correo'];
 
-//Insertar los datos en la base de datos
-$sql = "INSERT INTO usuarios (nombre, apellidoPaterno, apellidoMaterno, localidad ,fechaNacimiento, sexo)
-        VALUES ('$nombre', '$apellidoPaterno', '$apellidoMaterno', '$localidad','$fechaNacimiento', '$sexo')";
+// Preparar la consulta SQL para insertar los datos en la tabla 'usuarios'
+$sql = "INSERT INTO usuarios (nombre, apellidoPaterno, apellidoMaterno, fechaNacimiento, sexo, correo)
+        VALUES ('$nombre', '$apellidoPaterno', '$apellidoMaterno', '$fechaNacimiento', '$sexo', '$correo')";
 
+// Ejecutar la consulta SQL y se verifica si fue exitosa
 if ($conn->query($sql) === TRUE) {
     echo "Registro exitoso";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-// Cerrar conexión
+// cerrar la conexión a la base de datos
 $conn->close();
 ?>
